1ca7ee6dddaf8ee5bbcbfec5a921605a21ce83a3
suryansh173 <suryansh.vns149@gmail.com>
2026-05-26 12:26:44 +0530

    fastapi basics:'home, params, POST request'

---
+++ app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
+
+
+# Task 1 — Returning my name and college name
+@app.get("/")
+def home():
+    return {
+        "name": "Suryansh Pratap Singh",
+        "college": "Jaypee University of Engineering and Technology",
+        "message": "FastApi Learning"}
+
+# Task 2 — Product by ID (Path Parameter)
+@app.get("/product/{product_id}")
+def get_product(product_id: int):
+    return {
+        "product_id": product_id,
+        "name": "Sample Product",
+        "price": 999,
+        "in_stock": True}
+
+# Task 3 — Create Product (POST Request)
+class Product(BaseModel):
+    name: str
+    price: float
+
+@app.post("/create-product")
+def create_product(product: Product):
+    return {
+        "message": "Product created successfully!",
+        "product_name": product.name,
+        "product_price": product.price }
